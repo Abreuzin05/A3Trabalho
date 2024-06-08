a1e6b31fa5aa92215f7c86f148a94390e2df8339
JVreinaldo <158209614+JVreinaldo@users.noreply.github.com>
2024-06-08 17:13:48 -0300

    Update EmprestimoDAO.java

--- src/DAO/EmprestimoDAO.java
+++ src/DAO/EmprestimoDAO.java
@@ -12,14 +12,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-/**
- *
- * @author lucas
- */
 public class EmprestimoDAO {
 
     public static ArrayList<Emprestimo> MinhaLista = new ArrayList<Emprestimo>();
