af6f02401fddc0e92e27ea3129a7c86e85ceda30
Joao Vitor <146761120+Abreuzin05@users.noreply.github.com>
2024-06-08 17:08:55 -0300

    Update Amigo.java

--- src/Model/Amigo.java
+++ src/Model/Amigo.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Model;
 
 import java.sql.SQLException;
@@ -9,10 +6,6 @@ import java.util.ArrayList;
 
 import DAO.AmigoDAO;
 
-/**
- *
- * @author kaique
- */
 public class Amigo {
     private int id;
     private String nome;
