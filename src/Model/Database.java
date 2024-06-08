5e5dcdf83fafd0db1d627fcda626231be436ba3f
Joao Vitor <146761120+Abreuzin05@users.noreply.github.com>
2024-06-08 17:09:13 -0300

    Update Database.java

--- src/Model/Database.java
+++ src/Model/Database.java
@@ -1,17 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- *
- * @author lucas
- */
 public class Database {
 
     public static Connection getConexao() {
